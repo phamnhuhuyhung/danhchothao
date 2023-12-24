48f8d882e8d8f0d42beace615852cdd51de22520
hung <hung.pham@defide-ix.com>
2023-12-24 14:17:37 +0700

    fix code

---
+++ js/playImg.js
@@ -0,0 +1,83 @@
+
+var btn = document.getElementById("heartTxt");
+btn.style.opacity = 0;
+var btnVal = 0;
+var isPlayed = false;
+
+function showImage(){
+	//document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray[imageIndex];
+	//document.getElementById("imgTxt").style.opacity = 1 - flag;
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+		console.log(imageIndex)
+	}
+}
+
+function play(){
+	if(t == 0){
+		myImage.setAttribute("src", "");
+		myTxt.innerHTML = "";
+		imageIndex = 0;
+		clearInterval(showImageInterval);
+	}
+	if (!isPlayed) {
+		var audio = new Audio('Anh-Yeu-Em-Nhieu-Lam-Sy-Luan.mp3');
+		audio.play();
+		isPlayed = true;
+		btn.style.opacity = 0;
+	}
+	flag = 1 - flag;
+	document.getElementById("typeDiv").style.opacity = flag;
+	document.getElementById("imgTxt").style.opacity = 1 - flag;
+	if(t == 0){
+		//setTimeout(showImage, 1000);
+		setInterval(showImage, 6500);
+	}
+	t++;
+}
+
+function preshowImage(){
+	document.getElementById("imgTxt").style.opacity = 0;
+	myImage.setAttribute("src", imageArray[imageIndex]);
+	myTxt.innerHTML = txtArray[imageIndex];
+	imageIndex++;
+	if(imageIndex >= len){
+		imageIndex = 0;
+	}
+}
+
+function buttonFadeIn(){
+	if(btnVal < 1){
+		btnVal += 0.025;
+		btn.style.opacity = btnVal;
+	}
+	else{
+		clearInterval(buttonInterval);
+		if(ok == 3){
+			ok += 1;
+		}
+	}
+}
+
+
+
+function event(){
+
+	showImageInterval = setInterval(preshowImage, 100);
+
+	imgInterval = setInterval(function (){
+		if(ok == 3){
+			setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
+			clearInterval(imgInterval);
+		}
+	}, 50);
+}
+
+var showImageInterval;
+var imgInterval;
+var buttonInterval;
+
+event();
